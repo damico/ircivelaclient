800341f2bee1409b97a93ded887c562c72b6e3d6
jeffersoncarvalho <jeffersoncarvalho@273e37e2-bc85-11dd-b7f4-ab9653f8cbb3>
2008-12-09 20:03:52 +0000

    -Jtabbed panel -padrao observer -correção de pequenos bugs de interface

--- IRCIvelaClient/src/org/jdamico/ircivelaclient/listener/ListenConversation.java
+++ IRCIvelaClient/src/org/jdamico/ircivelaclient/listener/ListenConversation.java
@@ -1,23 +1,23 @@
 package org.jdamico.ircivelaclient.listener;
 
-import jerklib.Channel;
+import java.util.ArrayList;
+import java.util.Observer;
+
 import jerklib.ConnectionManager;
 import jerklib.Profile;
 import jerklib.Session;
 import jerklib.events.ConnectionCompleteEvent;
-import jerklib.events.IRCEvent;
 import jerklib.events.JoinCompleteEvent;
 import jerklib.events.JoinEvent;
 import jerklib.events.MessageEvent;
 import jerklib.events.QuitEvent;
-import jerklib.events.IRCEvent.Type;
 import jerklib.listeners.DefaultIRCEventListener;
-import jerklib.tasks.TaskImpl;
 
+import org.jdamico.ircivelaclient.observer.IRCObservable;
 import org.jdamico.ircivelaclient.view.HandleApplet;
 import org.jdamico.ircivelaclient.view.StaticData;
 
-public class ListenConversation extends DefaultIRCEventListener implements Runnable {
+public class ListenConversation extends DefaultIRCEventListener implements Runnable,IRCObservable {
 
 	/**
 	 * @param args
@@ -26,35 +26,28 @@ public class ListenConversation extends DefaultIRCEventListener implements Runna
 	
 	private MessageEvent jce = null;
 	private HandleApplet parent = null;
+	private ArrayList<Observer> observerList; 
 	
 	public Session session;
     
-	public Session getSession() {
-		return session;
-	}
-
-	public void setSession(Session session) {
-		this.session = session;
-	}
-
 	//TODO: change this as to not spam our channel
     static final String CHANNEL_TO_JOIN = StaticData.channel;
 	
 	public ListenConversation(HandleApplet parent){
 		
 		this.parent = parent;
+		this.observerList = new ArrayList<Observer>();
 
     }
-
-    
 
     public void run()
     {
     	
-    	String nick = StaticData.nick;
-		if(StaticData.nick.equals(StaticData.teacher)){
+    	
+		/*if(StaticData.nick.equals(StaticData.teacher)){
+			System.out.println("teste");
 			StaticData.nick = "|"+nick;
-		}
+		}*/
 		
 		
 		ConnectionManager manager = new ConnectionManager(new Profile(StaticData.nick, StaticData.nick, StaticData.nick, StaticData.nick));
@@ -64,43 +57,54 @@ public class ListenConversation extends DefaultIRCEventListener implements Runna
         session.addIRCEventListener(this);
 
         setSession(session);
+        System.out.println("session: " + session);
         
         
     }
     
-    protected void handleJoinCompleteEvent(JoinCompleteEvent event)
-    {
-        event.getChannel().say("Connected");
-        
-        Channel currentChannel = session.getChannel(StaticData.channel);
-        if(currentChannel!=null)
-        	parent.populateConnectedUsers(currentChannel);
-    }
+    //handlers
     
-    
-
     @Override
     protected void handleConnectComplete(ConnectionCompleteEvent event)
     {
         event.getSession().join(CHANNEL_TO_JOIN);
-        //System.out.println("conectado");
-        parent.enableControls();
+        System.out.println("conectado");
+        
+        /*parent.getChatPanel().enableControls();
         
         parent.setLoadingFlag(false);
+        */
+        this.notifyObservers(event);
         
+    }
+    
+  
+    protected void handleJoinCompleteEvent(JoinCompleteEvent event)
+    {
+        event.getChannel().say("Connected");
+        this.parent.getChatPanel().setSession(session);
         
+        /*Channel currentChannel = event.getChannel();
+        if(currentChannel!=null)
+        	parent.getChatPanel().populateConnectedUsers(currentChannel);
+        */
+        notifyObservers(event);
     }
 
     @Override
     protected void handleChannelMessage(MessageEvent event)
     {
     	 
-    	String tempStr = event.getNick() + ": " + event.getMessage();
+    	/*String tempStr = event.getNick() + ": " + event.getMessage();
         StaticData.chatMessage = tempStr;
         //System.out.println("teste--->"+event.getMessage());
-        String color = parent.getColor(event.getNick().trim());
-        parent.updateMainContentArea(tempStr,color);
-        setJce(event);
+        String color = parent.getChatPanel().getColor(event.getNick().trim());
+        parent.getChatPanel().updateMainContentArea(tempStr,color);
+        */
+    	notifyObservers(event);
+    	this.setJce(event);
+        
+    	
        
             try
             {
@@ -117,24 +121,56 @@ public class ListenConversation extends DefaultIRCEventListener implements Runna
     
     protected void handlePrivateMessage(MessageEvent event)
     {
-    	
-    	StaticData.chatMessage = event.getNick() + ": [private] " + event.getMessage();
-        setJce(event);
+    	this.notifyObservers(event);
+    	//StaticData.chatMessage = event.getNick() + ": [private] " + event.getMessage();
+    	this.setJce(event);
+        
     }
     
    
     @Override
     protected void handleJoinEvent(JoinEvent event) {
-    	 
-    	this.parent.addUserTable(event.getNick());
+    	 this.notifyObservers(event);
+    	 //this.parent.getChatPanel().addUserTable(event.getNick());
     }
     
     @Override
     protected void handleQuitEvent(QuitEvent event) {
     	//System.out.println(event.getNick()+" has left");
-    	this.parent.removeUserTable(event.getNick());
+    	this.notifyObservers(event);
+    	//this.parent.getChatPanel().removeUserTable(event.getNick());
+    	
     }
 
+	
+	
+	 
+	//Observer pattern methods
+	
+	@Override
+	public void addObserver(Observer observer) {
+		 
+		this.observerList.add(observer);
+	}
+
+	@Override
+	public void notifyObservers(Object arg) {
+		
+		for(Observer observer : this.observerList){
+			observer.update(null, arg);
+		}
+	}
+	
+	//set & gets
+	public Session getSession() {
+		return session;
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
+	}
+
+	//message
 	public MessageEvent getJce() {
 		return jce;
 	}
@@ -142,8 +178,6 @@ public class ListenConversation extends DefaultIRCEventListener implements Runna
 	public void setJce(MessageEvent jce) {
 		this.jce = jce;
 	}
-	 
-
 }
 
  
