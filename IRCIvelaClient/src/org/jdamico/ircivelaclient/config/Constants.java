d0486653017b7e465137e8ae8f82623f70cc889e
jeffersoncarvalho <jeffersoncarvalho@273e37e2-bc85-11dd-b7f4-ab9653f8cbb3>
2008-12-09 20:13:25 +0000

    modificando constante da versão

--- IRCIvelaClient/src/org/jdamico/ircivelaclient/config/Constants.java
+++ IRCIvelaClient/src/org/jdamico/ircivelaclient/config/Constants.java
@@ -3,7 +3,7 @@ package org.jdamico.ircivelaclient.config;
 public class Constants {
 	
 	public static final String APPNAME = "IRCIvelaClient";
-	public static final String APPVERSION = "0.0.44-SITH";
+	public static final String APPVERSION = "0.1.0-PALPATINE";
 	
 	public static final String PARAM_SERVER = "server";
 	public static final String PARAM_TEACHER = "teacher";
