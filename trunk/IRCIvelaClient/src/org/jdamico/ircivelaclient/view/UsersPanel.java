145654d62fa81c151d10b934c3e3c9b8dc443f16
jeffersoncarvalho <jeffersoncarvalho@273e37e2-bc85-11dd-b7f4-ab9653f8cbb3>
2009-01-08 19:43:55 +0000

    0.2.4-Captain Needa
    -pvt messages
    -gui main content
    -pequenos bugs na arqvore
    -florida

--- trunk/IRCIvelaClient/src/org/jdamico/ircivelaclient/view/UsersPanel.java
+++ trunk/IRCIvelaClient/src/org/jdamico/ircivelaclient/view/UsersPanel.java
@@ -1,18 +1,19 @@
 package org.jdamico.ircivelaclient.view;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Enumeration;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 
 import org.jdamico.ircivelaclient.config.Constants;
 
@@ -24,6 +25,10 @@ public class UsersPanel extends JPanel implements TreeSelectionListener, MouseLi
     private HandleApplet parent;
  
     private int childIndex = 0;
+    Icon rootImg = new ImageIcon(this.getClass().getResource("/org/jdamico/ircivelaclient/view/images/icon-forum.gif"));
+    Icon userImg = new ImageIcon(this.getClass().getResource("/org/jdamico/ircivelaclient/view/images/icon-online.gif"));
+    
+    
     
 	public UsersPanel(HandleApplet handleApplet) {
 		this.initializeTree();
@@ -31,6 +36,9 @@ public class UsersPanel extends JPanel implements TreeSelectionListener, MouseLi
 		this.parent = handleApplet;
 		//this.setSize(new Dimension(100,100));
 		this.add(this.usersPanel,BorderLayout.CENTER);
+		
+		
+
 	}
 
 	public void initializeTree(){
@@ -41,6 +49,13 @@ public class UsersPanel extends JPanel implements TreeSelectionListener, MouseLi
 		this.usersPanel.setScrollsOnExpand(true);
 		this.usersPanel.addTreeSelectionListener(this);
 		this.usersPanel.addMouseListener(this);
+		
+		DefaultTreeCellRenderer  rootRenderer = new DefaultTreeCellRenderer();
+		rootRenderer.setOpenIcon(rootImg);
+		rootRenderer.setClosedIcon(rootImg);
+		rootRenderer.setLeafIcon(userImg);
+		
+		this.usersPanel.setCellRenderer(rootRenderer);
 	}
 	
 	public void addUser(Object user){
@@ -50,18 +65,21 @@ public class UsersPanel extends JPanel implements TreeSelectionListener, MouseLi
 	}
 	
 	public void removeUser(String user){
+		 
 		int index = -1;
 		Enumeration<Object> children = this.root.children();
 		while(children.hasMoreElements()){
+			index++;
 			DefaultMutableTreeNode child = (DefaultMutableTreeNode)children.nextElement();
 			String userObject = (String)child.getUserObject();
 			if(userObject.equalsIgnoreCase(user))
 					break;
-			else
-				index++;
+			 
+				
 		}
 		if(index!=-1){
 			this.root.remove(index);
+			this.childIndex --;
 			this.usersPanel.updateUI();
 		}
 			
@@ -80,8 +98,12 @@ public class UsersPanel extends JPanel implements TreeSelectionListener, MouseLi
 		if(e.getClickCount()==2){
 			if(selectedUser.toString().equalsIgnoreCase(StaticData.nick))
 				JOptionPane.showMessageDialog(this, "You can not send private messages to yoursel. \n You are really a lonely person, aren't you?");
-			else
+			else{
+				if(selectedUser.toString().equalsIgnoreCase("channel"))
+					return;
 				parent.addTab(selectedUser.toString());
+			}
+				
 		}
 	}
 
