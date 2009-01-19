41094702b7f6d052e744f413d93e03d804934f44
jd.comment <jd.comment@273e37e2-bc85-11dd-b7f4-ab9653f8cbb3>
2009-01-19 16:43:07 +0000

    Fixed HashTable import

--- trunk/IRCIvelaClient/src/org/jdamico/ircivelaclient/config/Constants.java
+++ trunk/IRCIvelaClient/src/org/jdamico/ircivelaclient/config/Constants.java
@@ -2,10 +2,10 @@ package org.jdamico.ircivelaclient.config;
 
 import org.jdamico.ircivelaclient.view.StaticData;
 
-public class Constants {
+public interface Constants {
 	
 	public static final String APPNAME = "IRCIvelaClient";
-	public static final String APPVERSION = "0.2.5-Droideka";
+	public static final String APPVERSION = "0.2.6-Grievous";
 	
 	public static final String PARAM_SERVER = "server";
 	public static final String PARAM_TEACHER = "teacher";
